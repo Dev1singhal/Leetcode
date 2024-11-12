4e3f0b9ca00c684c54d73729f7d5f27041dc4abd
Dev1singhal <136137259+Dev1singhal@users.noreply.github.com>
2024-11-12 17:48:20 +0530

    Time: 94 ms (5.08%), Space: 44.2 MB (90.07%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.java
@@ -30,7 +30,7 @@ class Solution {
         int k=0;
         for(int i=2;i<=1000;i++){
             boolean flag=false;
-            for(int j=2;j*j<=i;j++){
+            for(int j=2;j<i;j++){
                 if(i%j==0){
                     flag=true;
                     break;
